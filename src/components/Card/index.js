fec45f099ec20775cbb3baded051a773208cd986
Tainá Santana <tainnaps@gmail.com>
2022-06-26 01:58:32 -0300

    style: add Card basic style

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
+
 const DEFAULT_IMG = 'https://cdn.pixabay.com/photo/2016/12/18/13/44/download-1915749_960_720.png';
 
 class Card extends React.Component {
   render() {
     const { props } = this;
     return (
-      <div className="card">
+      <div className="card column">
         <h3 data-testid="name-card">{ props.cardName }</h3>
         <img
           src={ !props.cardImage ? DEFAULT_IMG : props.cardImage }
@@ -15,22 +17,19 @@ class Card extends React.Component {
         />
         {props.cardTrunfo && <h4 data-testid="trunfo-card">Super Trunfo</h4>}
         <p data-testid="description-card">{ props.cardDescription }</p>
-        <div className="card-attributes">
-          <p className="attribute-name">
+        <p className="attribute-name">
           Attr01....................................
-            <span data-testid="attr1-card">{ props.cardAttr1 }</span>
-          </p>
-          <p className="attribute-name">
+          <span data-testid="attr1-card">{ props.cardAttr1 }</span>
+        </p>
+        <p className="attribute-name">
           Attr02....................................
-            <span data-testid="attr2-card">{ props.cardAttr2 }</span>
-          </p>
-          <p className="attribute-name">
+          <span data-testid="attr2-card">{ props.cardAttr2 }</span>
+        </p>
+        <p className="attribute-name">
           Attr03....................................
-            <span data-testid="attr3-card">{ props.cardAttr3 }</span>
-          </p>
-        </div>
+          <span data-testid="attr3-card">{ props.cardAttr3 }</span>
+        </p>
         <p data-testid="rare-card">{ props.cardRare }</p>
-        {props.cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
       </div>
     );
   }
